334bb3097aeaa7bb0ba55c6343e408b5a4062aac
Pakapon Kaewmark <sleep191@gmail.com>
2017-08-22 18:47:44 +0700

    Add new method for calculate an area of rectangle
    This method receive width and height of the rectangle from user input

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,24 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		greet("Pakapon Kaewmark");
+		getRectangleArea();
 	}
 	
 	private static void greet(String name){
 		System.out.println("Hello, Git");
 		System.out.println("I am " + name );
+	}
+	
+	private static void getRectangleArea(){
+		int width, height;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter rectangle width: ");
+		width = Integer.parseInt(scanner.nextLine());
+		System.out.println("Enter rectangle height: ");
+		height = Integer.parseInt(scanner.nextLine());
+		
+		System.out.println("The rectangle area is " + new Rectangle(width, height).getArea());
 	}
 }
