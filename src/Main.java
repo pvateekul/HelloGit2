5816fb05224b2dbe3bc101b4f6895de2b980dc23
Pakapon Kaewmark <sleep191@gmail.com>
2017-08-22 18:41:11 +0700

    Create class Main with greet() method for display greeting message

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		greet("Pakapon Kaewmark");
+	}
+	
+	private static void greet(String name){
+		System.out.println("Hello, Git");
+		System.out.println("I am " + name );
+	}
+}
